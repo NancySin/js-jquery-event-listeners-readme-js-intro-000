8fe8d01450c52fd8182e20ea9fef2ddad0994e11
nancy sin <nsin@sbcglobal.net>
2018-02-01 21:29:44 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -11,6 +11,22 @@ function getIt(){
    });
 }
 
+function pressIt() {
+   $('form').on("keydown", function(key){
+   if (key.which == 71) {
+      alert('you have pressed the G key')
+     }
+  });
+ }
+ 
+ function submitIt() {
+   $("form").on("submit", function() {
+    alert('Your form is going to be submitted now.');
+  });
+ }
+
+
+
 $(document).ready(function(){
 
 // call functions here
